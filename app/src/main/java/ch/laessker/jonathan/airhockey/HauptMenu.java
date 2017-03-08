6cf5d9c70866bdd8baac08b2c5e14f4a8d8d13cd
Jonathan Lässker <jonathan.laessker@students.ffhs.ch>
2017-03-08 19:58:25 +0100

    Single Player Button Prototyp implementiert

---
+++ app/src/main/java/ch/laessker/jonathan/airhockey/HauptMenu.java
@@ -0,0 +1,16 @@
+package ch.laessker.jonathan.airhockey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HauptMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_haupt_menu);
+    }
+
+
+    public void buttonPressed
+}
