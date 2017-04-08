d313987f56add90940b4f757b57137906b69bd1a
swisx <swisx@sunrise.ch>
2017-04-08 16:43:31 +0200

    Update: Navigation

--- app/src/main/java/ch/laessker/jonathan/airhockey/HauptMenu.java
+++ app/src/main/java/ch/laessker/jonathan/airhockey/HauptMenu.java
@@ -1,13 +1,56 @@
 package ch.laessker.jonathan.airhockey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class HauptMenu extends AppCompatActivity {
+
+    Button singlePlayerButton;
+    Button highScoreButton;
+    Button statisticButton;
+    Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_haupt_menu);
+
+        singlePlayerButton = (Button) findViewById(R.id.single_player_pre_game);
+
+        singlePlayerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final Intent i = new Intent(HauptMenu.this,SinglePlayerPreGame.class);
+                startActivity(i);
+            }
+        });
+
+        highScoreButton = (Button) findViewById(R.id.highscoreb);
+
+        highScoreButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final Intent i = new Intent(HauptMenu.this,HighScore.class);
+                startActivity(i);
+            }
+        });
+        statisticButton = (Button) findViewById(R.id.statisticb);
+
+        statisticButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final Intent i = new Intent(HauptMenu.this,Statistics.class);
+                startActivity(i);
+            }
+        });
+        settingsButton = (Button) findViewById(R.id.settingsb);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final Intent i = new Intent(HauptMenu.this,SettingsMenu.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
