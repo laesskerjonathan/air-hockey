493604bc9c4b8e5cafb65b19cbc4f27f62474398
Jonathan Lässker <jonathan.laessker@students.ffhs.ch>
2017-04-23 20:55:38 +0200

    Settings persistent in database

--- app/src/main/java/ch/laessker/jonathan/airhockey/AirHockeyDatenbank.java
+++ app/src/main/java/ch/laessker/jonathan/airhockey/AirHockeyDatenbank.java
@@ -12,15 +12,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class AirHockeyDatenbank extends SQLiteOpenHelper {
 
     private static final String DATENBANK_NAME = "settings.db";
-
     private static final int DATENBANK_VERSION = 1;
+    private final SQLiteDatabase db = this.getWritableDatabase();
 
-    public AirHockeyDatenbank(Context context){
+    private AirHockeyDatenbank(Context context){
         super(
                 context,
                 DATENBANK_NAME,
                 null,
                 DATENBANK_VERSION);
+    }
+
+    private static AirHockeyDatenbank sINSTANCE;
+    private static Object sLOCK = "";
+
+    public static AirHockeyDatenbank getInstance(Context context) {
+        if( sINSTANCE == null) {
+            synchronized(sLOCK) {
+                if (sINSTANCE == null && context != null ) {
+                    sINSTANCE = new AirHockeyDatenbank(context.getApplicationContext());
+                }
+            }
+        }
+        return sINSTANCE;
     }
 
     public void onCreate(SQLiteDatabase db){
@@ -42,121 +56,11 @@ public class AirHockeyDatenbank extends SQLiteOpenHelper {
                 "playTime INTEGER); ");
     }
 
-    public void onUpgrade(SQLiteDatabase db,
-                          int oldVersion,
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,
                           int newVersion) {
         db.execSQL("DROP TABLE settings;");
         db.execSQL("DROP TABLE saveGame;");
         onCreate(db);
-    }
-
-    public void updateSettings(SQLiteDatabase db,
-                               int effects,
-                               int soundtrack,
-                               int difficulty) {
-        db.execSQL("UPDATE settings " +
-                "SET effects = " + effects +
-                " soundtrack = " + soundtrack +
-                " difficulty = " + difficulty +
-                " WHERE id = 1;");
-    }
-
-    private int getSettingsField(SQLiteDatabase db,
-                                String field) {
-        int ergebnis = 1;
-        Cursor effects = db.query(
-                false,
-                "settings",
-                new String[] {
-                        field
-                },
-                "id = ?",
-                new String[] {
-                        "1"
-                },
-                null, null, null, null
-        );
-        while( effects.moveToNext()) {
-            ergebnis = effects.getInt(0);
-        }
-        return ergebnis;
-    }
-
-    public int getEffects(SQLiteDatabase db) {
-        return getSettingsField(db, "effects");
-    }
-
-    public int getSoundtrack(SQLiteDatabase db) {
-        return getSettingsField(db, "soundtrack");
-    }
-
-    public int getDifficulty(SQLiteDatabase db) {
-        return getSettingsField(db, "difficulty");
-    }
-
-    public void saveGame(SQLiteDatabase db,
-                        int playerPoints,
-                        int enemyPoints,
-                        int time){
-        db.execSQL("INSERT INTO saveGame (playerPoints, enemyPoints, playTime) " +
-                "VALUES (" + playerPoints + ", " + enemyPoints + ", " + time + ");");
-    }
-
-    private int getStatsField(SQLiteDatabase db,
-                                 String field) {
-        int ergebnis = 1;
-        Cursor effects = db.query(
-                false,
-                "saveGame",
-                new String[] {
-                        "SUM(" + field +")"
-                },
-                "id = ?",
-                new String[] {
-                        "1"
-                },
-                null, null, null, null
-        );
-        while( effects.moveToNext()) {
-            ergebnis = effects.getInt(0);
-        }
-        return ergebnis;
-    }
-
-
-    public int getPlayerPoints(SQLiteDatabase db) {
-        return getSettingsField(db, "playerPoints");
-    }
-
-    public int getEnemyPoints(SQLiteDatabase db) {
-        return getSettingsField(db, "enemyPoints");
-    }
-
-    public int getTime(SQLiteDatabase db) {
-        return getSettingsField(db, "playTime");
-    }
-
-    private HighScorePoints getHighscoreField(SQLiteDatabase db,
-                                 String field) {
-        int ergebnis = 1;
-        Cursor highScore = db.query(
-                false,
-                "saveGame",
-                new String[] {
-                    "name",
-                    "playerPoints"
-                },
-                null,
-                null,
-                null, null,
-                "playerPoints DESC", //ORDER BY
-                "3" //Limit
-        );
-        HighScorePoints points = new HighScorePoints();
-        while( highScore.moveToNext()) {
-            points.setEntry(highScore.getInt(0), highScore.getString(0));
-        }
-        return points;
     }
 
 
--- app/src/main/java/ch/laessker/jonathan/airhockey/SettingsMenu.java
+++ app/src/main/java/ch/laessker/jonathan/airhockey/SettingsMenu.java
@@ -1,12 +1,35 @@
 package ch.laessker.jonathan.airhockey;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class SettingsMenu extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,16 +37,115 @@ public class SettingsMenu extends AppCompatActivity {
         setContentView(R.layout.activity_settings_menu);
 
 
-        Button saveSettingsButton = (Button) findViewById(R.id.single_player_pre_game);
+        final AirHockeyDatenbank AHDB = AirHockeyDatenbank.getInstance(getApplicationContext());
+        final SQLiteDatabase db = AHDB.getWritableDatabase();
+
+        String[] projection = {
+                "effects",
+                "soundtrack",
+                "difficulty"
+        };
+        String selection = "id" + " = ?";
+        String[] selectionArgs = { "1" };
+        String sortOrder =
+                "effects" + " DESC";
+
+        Cursor cursor = db.query(
+                "settings",                     // The table to query
+                projection,                               // The columns to return
+                selection,                                // The columns for the WHERE clause
+                selectionArgs,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+
+        int effects = 10;
+        int soundtrack = 10;
+        int difficulty = 10;
+
+        while(cursor.moveToNext()) {
+             effects = cursor.getInt(0);
+             soundtrack = cursor.getInt(1);
+             difficulty = cursor.getInt(2);
+        }
+        cursor.close();
+        Toast.makeText(SettingsMenu.this, "Settings Loaded: effects: " + effects + ", soundtrack: " + soundtrack + ", difficulty: " + difficulty, Toast.LENGTH_LONG).show();
+
+        Button saveSettingsButton = (Button) findViewById(R.id.button);
+
+        final Switch effectSwitch = (Switch) findViewById(R.id.switch_effects);
+        final Switch soundtrackSwitch = (Switch) findViewById(R.id.switch_soundtrack);
+        final Spinner difficultySpinner = (Spinner) findViewById(R.id.spinner_difficulty);
+
+        if(effects == 1) {
+            effectSwitch.setChecked(true);
+        }
+        if(soundtrack == 1) {
+            soundtrackSwitch.setChecked(true);
+        }
+        difficultySpinner.setSelection(difficulty);
 
         saveSettingsButton.setOnClickListener(new View.OnClickListener() {
-            //TODO: Load Settings from Database
             public void onClick(View v) {
-                //TODO: Save Settings in Database
-                final Intent i = new Intent(SettingsMenu.this,HauptMenu.class);
+                int effects = (effectSwitch.isChecked() ? 1 : 0);
+                int soundtrack = (soundtrackSwitch.isChecked() ? 1 : 0);
+                int difficulty = difficultySpinner.getSelectedItemPosition();
+
+                ContentValues values = new ContentValues();
+                values.put("effects", effects);
+                values.put("soundtrack", soundtrack);
+                values.put("difficulty", difficulty);
+
+                String selection = "id" + " = ?";
+                String[] selectionArgs = { "1" };
+
+                long newRowId = db.update("settings", values, selection, selectionArgs);
+
+                Toast.makeText(SettingsMenu.this, "Settings Saved: effects: " + effects + ", soundtrack: " + soundtrack + ", difficulty: " + difficulty, Toast.LENGTH_LONG).show();
+                final Intent i = new Intent(SettingsMenu.this, HauptMenu.class);
                 startActivity(i);
             }
         });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("SettingsMenu Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
