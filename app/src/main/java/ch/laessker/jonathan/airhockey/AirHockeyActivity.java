2c4a56672e084868178320a7104970effc4b2240
swisx <swisx@sunrise.ch>
2017-06-05 15:42:15 +0200

    Update Player 1 touch listener enables

--- app/src/main/java/ch/laessker/jonathan/airhockey/AirHockeyActivity.java
+++ app/src/main/java/ch/laessker/jonathan/airhockey/AirHockeyActivity.java
@@ -10,73 +10,127 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class AirHockeyActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
+public class AirHockeyActivity extends Activity {
+    /**
+     * Hold a reference to our GLSurfaceView
+     */
     private GLSurfaceView glSurfaceView;
     private boolean rendererSet = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Setup GLSurfaceView
         glSurfaceView = new GLSurfaceView(this);
 
-        // Check for OpenGL ES 3 Support
-        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
-        // Everything after || is for checking if running on emulator
-        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000 ||
-                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1 &&
-                        (Build.FINGERPRINT.startsWith("generic") || Build.FINGERPRINT.startsWith(" unknown") ||
-                                Build.MODEL.contains("google_sdk") || Build.MODEL.contains("Emulator") ||
-                                Build.MODEL.contains("Android SDK built for x86")));
+        // Check if the system supports OpenGL ES 2.0.
+        ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager
+                .getDeviceConfigurationInfo();
+        // Even though the latest emulator supports OpenGL ES 2.0,
+        // it has a bug where it doesn't set the reqGlEsVersion so
+        // the above check doesn't work. The below will detect if the
+        // app is running on an emulator, and assume that it supports
+        // OpenGL ES 2.0.
+        final boolean supportsEs2 =
+                configurationInfo.reqGlEsVersion >= 0x20000
+                        || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                        && (Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86")));
 
+        final AirHockeyRenderer airHockeyRenderer = new AirHockeyRenderer(this);
 
         if (supportsEs2) {
-            // Request an OpenGL ES 3.0 compatible context.
-            glSurfaceView.setEGLContextClientVersion(3);
+            // ...
+            // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+
             // Assign our renderer.
-            glSurfaceView.setRenderer(new AirHockeyRenderer(this));
+            glSurfaceView.setRenderer(airHockeyRenderer);
             rendererSet = true;
         } else {
-            Toast.makeText(this, "This device does not support OpenGL ES 3.0.", Toast.LENGTH_LONG).show();
+            /*
+             * This is where you could create an OpenGL ES 1.x compatible
+             * renderer if you wanted to support both ES 1 and ES 2. Since
+             * we're not doing anything, the app will crash if the device
+             * doesn't support OpenGL ES 2.0. If we publish on the market, we
+             * should also add the following to AndroidManifest.xml:
+             *
+             * <uses-feature android:glEsVersion="0x00020000"
+             * android:required="true" />
+             *
+             * This hides our app from those devices which don't support OpenGL
+             * ES 2.0.
+             */
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+                    Toast.LENGTH_LONG).show();
             return;
         }
+
+        glSurfaceView.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event != null) {
+                    // Convert touch coordinates into normalized device
+                    // coordinates, keeping in mind that Android's Y
+                    // coordinates are inverted.
+                    final float normalizedX =
+                            (event.getX() / (float) v.getWidth()) * 2 - 1;
+                    final float normalizedY =
+                            -((event.getY() / (float) v.getHeight()) * 2 - 1);
+
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        glSurfaceView.queueEvent(new Runnable() {
+                            @Override
+                            public void run() {
+                                airHockeyRenderer.handleTouchPress(
+                                        normalizedX, normalizedY);
+                            }
+                        });
+                    } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                        glSurfaceView.queueEvent(new Runnable() {
+                            @Override
+                            public void run() {
+                                airHockeyRenderer.handleTouchDrag(
+                                        normalizedX, normalizedY);
+                            }
+                        });
+                    }
+
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
 
         setContentView(glSurfaceView);
     }
 
-    /**
-     * Dispatch onPause() to fragments.
-     */
     @Override
     protected void onPause() {
         super.onPause();
 
-        // Need to handle lifecycle to avoid crash from OpenGL
         if (rendererSet) {
             glSurfaceView.onPause();
         }
     }
 
-    /**
-     * Dispatch onResume() to fragments.  Note that for better inter-operation
-     * with older versions of the platform, at the point of this call the
-     * fragments attached to the activity are <em>not</em> resumed.  This means
-     * that in some cases the previous state may still be saved, not allowing
-     * fragment transactions that modify the state.  To correctly interact
-     * with fragments in their proper state, you should instead override
-     * {@link #onResumeFragments()}.
-     */
     @Override
     protected void onResume() {
         super.onResume();
 
-        // Need to handle lifecycle to avoid crash from OpenGL
         if (rendererSet) {
             glSurfaceView.onResume();
         }
     }
-
 }
