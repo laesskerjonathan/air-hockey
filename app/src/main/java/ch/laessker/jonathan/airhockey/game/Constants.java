0ecca8a0541ea2d71a6423f6b98965bcc9d6bce5
swisx <swisx@sunrise.ch>
2017-06-05 18:32:59 +0200

    Delete old unused Classes

--- app/src/main/java/ch/laessker/jonathan/airhockey/game/Constants.java
+++
@@ -1,9 +0,0 @@
-package ch.laessker.jonathan.airhockey.game;
-
-public class Constants {
-
-	// Environment Constants
-	public static float restitution = 0.85f;
-	public static float epsilon = 0.000009f;
-
-}
